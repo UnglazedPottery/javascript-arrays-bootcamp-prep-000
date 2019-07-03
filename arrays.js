0d8c901c77f7411428c35b131d1d6af3c94295fa
Jade Huynh <35jadeh@gmail.com>
2019-07-03 17:50:21 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,50 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element)
+{
+  array = [element,...array]
+  return array 
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element)
+{
+  array.unshift(element)
+  return array
+}
+
+function addElementToEndOfArray(array, element)
+{
+  array = [...array, element]
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element)
+{
+  array.push(element)
+  return array
+}
+
+function accessElemntInArray(element, array)
+{
+  
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(element, array)
+{
+  
+}
+
+function RemoveFromElementBeginningOfArray()
+{
+  
+}
+
+function destructivelyRemoveElementFromEndOfArray()
+{
+  
+}
+
+function RemoveElementFromEndOfArray()
+{
+  
+}
